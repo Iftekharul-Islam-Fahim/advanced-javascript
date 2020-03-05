618195c3e0a9d487fefc824e2263cfa2afd8dba1
Iftekharul Islam <iftekharulislam1110@gmail.com>
2020-03-06 00:30:49 +0600

    array slice, array splice and array join

---
+++ array-split.js
@@ -0,0 +1,13 @@
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+//slice(startIndex, endIndex)
+const part = nums.slice(0, 4);
+console.log(part);
+
+//splice(startIndex, deleteCount, numbers[])
+const removed = nums.splice(0, 5, 11, 22, 33, 44, 55);
+console.log(removed);
+console.log(nums);
+
+const together = nums.join(", ");
+console.log(together);
